504264742185fd64990e8f464070cd6f342fa709
Ayush Kumar <kumarayush731@gmail.com>
2020-07-18 07:19:22 +0530

    sign in and sign up page added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,3 +6,30 @@ module.exports.profile=function(req,res){
 
     // return res.send('<h1>profile is running</h1>');
 }
+
+// render the sign up page
+module.exports.signUp=function(req,res){
+    
+    return res.render('user_sign_up',{
+        title:"sign up"
+    });
+}
+
+// render the sign in page
+
+module.exports.signIn=function(req,res){
+    
+    return res.render('user_sign_in',{
+        title:"sign in"
+    });
+}
+
+//get the signup data
+module.exports.create=function(req,res){
+    //todo later
+}
+
+//sign in and create a session for the user
+module.exports.createSession=function(req,res){
+    //todo later
+}
