acd20ac17f6059b83c10db90f86a6b7445d202ad
Ayush Kumar <kumarayush731@gmail.com>
2020-07-21 17:53:54 +0530

    display posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,26 @@
+const Post=require('../models/post');
+
+
 module.exports.home=function(req,res){
 
-    return res.render('home',{
-        title:"Home Page"
-    });
+    // Post.find({},function(err,posts){
+
+    //     return res.render('home',{
+    //             title:"Home Page",
+    //             posts:posts
+    //         });
+    // });
+
+    //populate the user for each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Home Page",
+            posts:posts
+        });
+    })
+
+    // return res.render('home',{
+    //     title:"Home Page"
+    // });
     // return res.end("<h1>controller is running</h1>");
 }
