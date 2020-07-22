6ff1ad0952f9de3a1ce5176d04256b121dcd5f12
Ayush Kumar <kumarayush731@gmail.com>
2020-07-22 10:07:23 +0530

    adding comments to the database

--- models/comment.js
+++ models/comment.js
@@ -1,26 +1,23 @@
 const mongoose = require('mongoose');
 
-const commentSchema=new mongoose.Schema({
-    content:{
-        type:String,
-        required:true
-    },
 
-    //commets belongs to the user
-    
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
     },
-    post:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Post'
+    // comment belongs to a user
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
     }
-
 },{
-    timestamps:true
+    timestamps: true
 });
 
-const comment=mongoose.model('comment',commentSchema);
-
-module.exports=comment;
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
--- models/post.js
+++ models/post.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 
-const postSchema=new mongoose.Schema({
 
-    content:{
-        type:String,
-        required:true
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
     },
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
+    user: {
+        type:  mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+
     },
-    //include the array od ids of all comment in this post schema itself
-    comments:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'comment'
-    }
+    // include the array of ids of all comments in this post schema itself
+    comments: [
+        {
+            type:  mongoose.Schema.Types.ObjectId,
+            ref: 'comment'
+        }
+    ]
 },{
-    timestamps:true
+    timestamps: true
 });
 
-const Post=mongoose.model('Post',postSchema);
-module.exports=Post;
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
