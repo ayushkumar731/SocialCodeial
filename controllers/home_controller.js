5e1be6f74c996651f9295083c194e3e85941e7ef
Ayush Kumar <kumarayush731@gmail.com>
2020-07-22 10:53:05 +0530

    nested population :: display comments on homepage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -12,12 +12,22 @@ module.exports.home=function(req,res){
     // });
 
     //populate the user for each post
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    
+    .exec(function(err,posts){
         return res.render('home',{
             title:"Home Page",
             posts:posts
         });
     })
+
 
     // return res.render('home',{
     //     title:"Home Page"
