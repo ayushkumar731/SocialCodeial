1ca6adfbe0be7423d99e1f23e3e6eb80c18b6499
Ayush Kumar <kumarayush731@gmail.com>
2020-07-30 12:01:05 +0530

    installl and seeting up kue and using it

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,7 +1,9 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer=require('../mailers/comments_mailer');
-
+const kue=require('../config/kue');
+const commentEmailWorker=require('../workers/comment_email_workers');
+const queue = require('../config/kue');
 module.exports.create = async function(req, res){
 
     try{
@@ -21,7 +23,14 @@ module.exports.create = async function(req, res){
 
              // Similar for comments to fetch the user's id!
              comment = await comment.populate('user', 'name email').execPopulate();
-             commentsMailer.newComment(comment);
+            //  commentsMailer.newComment(comment);
+            let job=queue.create('emails',comment).save(function(err){
+                if(err){
+                    console.log('error in sending to the  queue');
+                    return;
+                }
+                console.log('job enqued',job.id);
+            });
             if (req.xhr){
                
     
