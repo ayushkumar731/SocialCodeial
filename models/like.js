99b0be5128fde2a5babc5850798307a064673ae3
Ayush Kumar <kumarayush731@gmail.com>
2020-07-31 07:05:44 +0530

    Schema Setup:: Likes

---
+++ models/like.js
@@ -0,0 +1,24 @@
+const mongoose=require('../config/mongoose');
+
+const likeSchema=new mongoose.Schema({
+    user: {
+        type:mongoose.Schema.objectId
+    },
+    //this defines the object id of the liked object
+    likeable: {
+        type: mongoose.Schema.objectId,
+        reuired:true,
+        refPath:'onModel'
+    },
+    // this field is used for defining the type of the liked object this is a dynamic reference
+    onModel: {
+        type: String,
+        reuired: true,
+        enum: ['Post', 'Comment']
+    }
+},{
+        timeStamp:true
+});
+
+const Like=mongoose.model('Like',likeSchema);
+module.exports=Like;
